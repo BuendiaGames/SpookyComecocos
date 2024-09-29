6a148762ab7de26a3e5fcbfbb5af75aef1f73929
Mapachana <anabuenrua99@gmail.com>
2024-09-29 16:24:24 +0200

    Terminado zombi

--- core/characters/zombi/zombi.gd
+++ core/characters/zombi/zombi.gd
@@ -1,28 +1,32 @@
 extends CharacterBody3D
 
+#Velocidad de movimiento del vampiro
+const speed = 50.0
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+#Referencias al jugador
+var player = null
+
+#Coge las instancias del jugador y teletransprtadores
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	$main_mesh/AnimationPlayer.play("walk")
 
 
 func _process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
+	#Coge la direccion a la proxima pista del navmesh, hacia el jugador
+	var direction = $NavigationAgent3D.get_next_path_position() - position
+	direction.y = 0. 
+	velocity = delta * speed * direction.normalized()
+	
+	#Rotacion hacia donde esta andando
+	var look_direction = Vector2(velocity.z, velocity.x)
+	$main_mesh.rotation.y = look_direction.angle()
+	
+	#Mover y colisionar
 	move_and_slide()
+
+
+func _on_update_target_timeout() -> void:
+	$NavigationAgent3D.target_position = player.position
+	
