1c6dcacecca20c46b7eed8ae0e0243adcd741a25
Mapachana <anabuenrua99@gmail.com>
2024-10-20 20:39:24 +0200

    Añado controles al menu

--- core/menu/mainmenu.gd
+++ core/menu/mainmenu.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var showing_credits = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,9 +14,9 @@ func _on_start_pressed() -> void:
 	GameManager.load_game("res://core/levels/laberinto_prueba.tscn")
 
 func _on_credits_pressed() -> void:
-	showing_credits = true
 	$backgroundtext/columns/center/start.hide()
 	$backgroundtext/columns/center/credits.hide()
+	$backgroundtext/columns/center/howtoplay.hide()
 	
 	$backgroundtext/columns/center/credits_text.show()
 	$backgroundtext/columns/center/back.show()
@@ -26,7 +24,21 @@ func _on_credits_pressed() -> void:
 func _on_back_pressed() -> void:
 	$backgroundtext/columns/center/start.show()
 	$backgroundtext/columns/center/credits.show()
+	$backgroundtext/columns/center/howtoplay.show()
+
 	
 	$backgroundtext/columns/center/credits_text.hide()
+	$backgroundtext/columns/center/howtoplay_text.hide()
 	$backgroundtext/columns/center/back.hide()
 	
+
+
+func _on_howtoplay_pressed() -> void:
+
+	$backgroundtext/columns/center/start.hide()
+	$backgroundtext/columns/center/credits.hide()
+	$backgroundtext/columns/center/howtoplay.hide()
+	
+	$backgroundtext/columns/center/howtoplay_text.show()
+	$backgroundtext/columns/center/back.show()
+	pass # Replace with function body.
